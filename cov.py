2bfcd5a182b261bbabda744363f67f87776cf6e4
rchui <ryan.w.chui@gmail.com>
2017-04-29 13:48:33 -0500

    added covariate generation file

---
+++ cov.py
@@ -0,0 +1,51 @@
+import csv
+import sys
+
+csv.field_size_limit(sys.maxsize)
+
+"""Reads in data passed by the user from a CSV file."""
+count = 0
+fileName = sys.argv[1]
+csvArray = {}
+with open(fileName) as csvFile:
+	reader = csv.reader(csvFile)
+	for row in reader:
+		row.pop()
+		if count != 0:
+			readRow = [i for i in row]
+			csvArray[readRow[2]] = readRow
+		count += 1
+	csvFile.close()
+
+"""Find rows to remove."""
+rmArray = []
+for i in csvArray.keys():
+	if csvArray[i][12] == '':
+		csvArray[i][12] = -9
+
+"""Read in ped file"""
+pedArray = []
+for line in open(sys.argv[2]):
+	pedArray.append(line.split())
+
+for row in pedArray:
+	if csvArray[row[1]][5] == 1:
+		row[4] = 2
+	else:
+		row[4] = 1
+
+	if csvArray[row[1]][12] == 0:
+		row[5] = 1
+	else:
+		row[5] = 2
+
+"""Write to covariate file"""
+file = open(sys.argv[3], 'a')
+output = ""
+for row in pedArray:
+	output = str(row[0])
+	for i in range(len(row)):
+		if i != 0:
+			output += " " + str(row[i])
+	output += "\n"
+	file.write(output)
