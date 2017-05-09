0f22848ecfff6fce25d86d5db76e854016120df9
rchui <ryan.w.chui@gmail.com>
2017-05-09 14:11:40 -0700

    Added more tracking prints

--- cov.py
+++ cov.py
@@ -4,7 +4,7 @@ import sys
 csv.field_size_limit(sys.maxsize)
 
 """Reads in data passed by the user from a CSV file."""
-print("Reading in csv data...")
+print("[0%] ... Reading in csv data")
 count = 0
 fileName = sys.argv[1]
 csvArray = {}
@@ -19,20 +19,28 @@ with open(fileName) as csvFile:
     csvFile.close()
 
 """Find rows to remove."""
-print("Removing empty PTSD outcome rows...")
+print("[25%] ... Marking empty PTSD outcome as missing...")
 rmArray = []
+count = 0
 for i in csvArray.keys():
     if csvArray[i][12] == '':
+        count += 1
 	csvArray[i][12] = -9
+    if csvArray[i][9] == "":
+        count += 1
+        csvArray[i][9] = -9
+print("    Marked " + count + " rows")
 
 """Read in ped file"""
-print("Reading in ped file...")
+print("[50%] ... Reading in ped file...")
 pedArray = []
+count = 0
 for line in open(sys.argv[2]):
+    print("    Reading in row " + count)
     pedArray.append(line.split())
 
 for row in pedArray:
     print(row[0:2])
 
 """Write to covariate file"""
-print("Writing to covariate file...")
+print("[75%] ... Writing to covariate file...")
